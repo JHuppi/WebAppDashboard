d61b05c1441acc31a0a15e2ec1597af19f8e7c8d
Jono Hupp <jhuppi@hotmail.com>
2016-04-07 16:54:36 -0600

    Added New Member and Recent Activity Widgets

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ var dayData = {
             data: [11,3,18,14,16,17,11]
         }
     ]
-}
+};
 
 var weeklyChart = new Chart(dayCanvas).Bar(dayData);
 
@@ -61,7 +61,7 @@ var mobileData = [
         color: "#7477bf",
         label: "Desktop"
     }
-]
+];
 
 var mobileChart = new Chart(mobileCanvas).Doughnut(mobileData,
                         {segmentShowStroke: false,
@@ -75,6 +75,106 @@ var socialStats = {twitter: 250, facebook: 300, gplus: 120};
 $("#twitterStats").append('<span class="socialNum">' + socialStats.twitter + '</span>');
 $("#faceStats").append('<span class="socialNum">' + socialStats.facebook + '</span>');
 $("#gplusStats").append('<span class="socialNum">' + socialStats.gplus + '</span>');
+
+/*New Member Widget*/
+var newMembers = [
+        {
+            "name": "Victoria Chambers",
+            "email": "victoria.chambers80@example.com",
+            "joinDate": "2016-01-27",
+            "img": "img/vchamber.jpg",
+            "mostRecentAct": "signUp"
+        },
+        {
+            "name": "Dale Byrd",
+            "email": "dale.byrd52@example.com",
+            "joinDate": "2016-01-23",
+            "img": "img/dbyrd.jpg",
+            "mostRecentAct": "signUp"
+        },
+        {
+            "name": "Dawn Wood",
+            "email": "dawn.wood16@example.com",
+            "joinDate": "2016-01-20",
+            "img": "img/dwood.jpg",
+            "mostRecentAct": "post"
+        },
+        {
+            "name": "Dan Oliver",
+            "email": "dan.oliver82@example.com",
+            "joinDate": "2016-01-14",
+            "img": "img/doliver.jpg",
+            "mostRecentAct": "comment"
+        }
+];
+
+$.each(newMembers, function() {
+    var $memberDiv = $('<div class="memberDiv"></div>');
+    var $memberImg = $('<img class="memberImg">');
+    var $memberInfo = $('<div class="memberInfo"></div>');
+    var $memberName = $('<div class="memberName"></div>');
+    var $memberEMail = $('<div class="memberEMail"></div>');
+    var $memberJoinDate = $('<div class="memberJoinDate"></div>');
+
+        $memberImg.attr("src", this.img);
+        $memberDiv.append($memberImg);
+        
+        $memberName.text(this.name);
+        $memberInfo.append($memberName);
+        
+        $memberEMail.text(this.email);
+        $memberInfo.append($memberEMail);
+        
+        $memberInfo.appendTo($memberDiv);
+        
+        $memberJoinDate.text(this.joinDate);
+        $memberDiv.append($memberJoinDate);
+        
+        $memberDiv.appendTo("#newMembers");
+});
+
+$.each(newMembers, function(){
+    var $activityDiv = $('<div class="activityDiv"></div>');
+    var $memberImg = $('<img class="memberImg">');
+    var $memberActivity = $('<div class="memberActivity"></div>');
+    var $activityIcon = $('<img class="activityIcon">');
+    
+    if (this.mostRecentAct === "signUp") {
+        $memberImg.attr("src", this.img);
+        $activityDiv.append($memberImg);
+        
+        $memberActivity.text(this.name + " recently signed up.");
+        $activityDiv.append($memberActivity);
+        
+        $activityIcon.attr("src","svg/icon-pencil.svg");
+        $activityDiv.append($activityIcon); 
+        
+        $activityDiv.appendTo("#recentAct");
+    } else if (this.mostRecentAct === "post") {
+       $memberImg.attr("src", this.img);
+        $activityDiv.append($memberImg);
+        
+        $memberActivity.text(this.name + " recently made a post");
+        $activityDiv.append($memberActivity);
+        
+        $activityIcon.attr("src","svg/icon-post.svg");
+        $activityDiv.append($activityIcon); 
+        
+        $activityDiv.appendTo("#recentAct");
+    } else {
+        
+        $memberImg.attr("src", this.img);
+        $activityDiv.append($memberImg);
+        
+        $memberActivity.text(this.name + " recently commented on a post.");
+        $activityDiv.append($memberActivity);
+        
+        $activityIcon.attr("src","svg/icon-comment.svg");
+        $activityDiv.append($activityIcon);
+        
+        $activityDiv.appendTo("#recentAct");
+    }
+});
 
 /*Message Widget*/
 var messageForm = document.getElementById("messageForm");
@@ -91,11 +191,13 @@ $mConfirmation.append($escape);
 $mConfirmation.append($message);
 $("body").append($overlay);
 
-
 $("#send").click(function() {
     $("#messageForm:input, #messageForm textarea").each(function() {
         if($(this).val() !== '') {
             $message.text('Your message "' + message.value + '" has been sent to: ' + userName.value);
+            $overlay.css("display","flex");
+        } else {
+            $message.text('Your message was NOT sent. You are missing either the recipient or the message.');
             $overlay.css("display","flex");
         }
     });
